b2688a2e08e3b094d94e41883291c8665cb1fd3c
ZhenxuanZhang-Jensen <1157911514.com>
2022-11-25 12:41:11 +0100

    try stack

--- run_make_plotCard.sh
+++ run_make_plotCard.sh
@@ -1,2 +1,2 @@
 # ./heppi-make-plotcard --load  /eos/user/z/zhenxuan/Hgg_mass/MiniTree/UL2016/preVFP/SingleEleData_preVFP_Zee_BDT_ptWgt.root --out   plotcardUL16PreVFP_zee.json --tree  Data_13TeV_UntaggedTag
-./heppi-make-plotcard --load  /eos/user/z/zhenxuan/Hgg_mass/MiniTree/UL2018/MassUL2018_ETSS/DYMC_Summer19UL2018_ZeeTree_Syst_BDT.root --out   plotcardUL16PreVFP_zee.json --tree  Data_13TeV_UntaggedTag
+./heppi-make-plotcard --load  /eos/user/z/zhenxuan/Hgg_mass/MiniTree/UL2018/MassUL2018_ETSS/DYMC_Summer19UL2018_ZeeTree_Syst_BDT_ptWgt_all.root --out plotcardUL18.json --tree DYToLL_13TeV_UntaggedTag
---
+++ run_plot_UL18_Zee.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+# input variables validation
+# ./heppi-draw -p plotcardUL18.json -o "/eos/user/z/zhenxuan/Hgg_mass/MiniTree/UL2018/MassUL2018_ETSS/" -s "/eos/user/z/zhenxuan/Hgg_mass/MiniTree/UL2018/MassUL2018_ETSS/" -t {sampleid}_13TeV_UntaggedTag --label zee -v DiphotonMVA_self
+./heppi-draw -p plotcardUL18.json -o "/eos/user/z/zhenxuan/Hgg_mass/MiniTree/UL2018/MassUL2018_ETSS/" -s "/eos/user/z/zhenxuan/Hgg_mass/MiniTree/UL2018/MassUL2018_ETSS/" -t {sampleid}_13TeV_UntaggedTag --label zee -v sigmawv
